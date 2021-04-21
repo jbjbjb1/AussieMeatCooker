38fd3a37be5a6fc928603b3a367c3caea59d08a9
jbjbjb1 <johnpbills@gmail.com>
2021-04-21 15:55:42 +0800

    updates to plot data with python

--- Python scripts/plot_serial_usblog.py
+++ Python scripts/plot_serial_usblog.py
@@ -11,11 +11,17 @@ class BbqPlot:
         data = []
         with open(file, 'r') as f:
             for line in f:
-                time = pd.to_datetime(line[:12], format='%H:%M:%S.%f')
-                air = float(line.split('C')[0].split(':')[-1].strip())
-                prb1 = float(line.split(':')[-1][:-2].strip())
-                data.append({'Time': time, 'Air': air, 'Prb1': prb1})
-                #print({'Time': time, 'Air': air, 'Prb1': prb1})
+                if '@' not in line:
+                    try:
+                        time = pd.to_datetime(line[:12], format='%H:%M:%S.%f')
+                        air = float(line.split('C')[0].split(':')[-1].strip())
+                        prb1 = float(line.split(':')[-1][:-2].strip())
+                        data.append({'Time': time, 'Air': air, 'Prb1': prb1})
+                        #print({'Time': time, 'Air': air, 'Prb1': prb1})
+                    except ValueError:
+                        continue
+                else:
+                    continue
         self.df = pd.DataFrame(data)
 
     def plot(self):
@@ -25,9 +31,10 @@ class BbqPlot:
         fig.autofmt_xdate()
         ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
         ax.set_title('Time Temperature Plot')
+        plt.ylim((0,220))
         plt.show()
 
 
 
-a = BbqPlot(r"Python scripts\sample data\serial_20210130_085518.txt")
+a = BbqPlot(r"Python scripts\sample data\serial_20210312_174735_steaks.txt")
 a.plot()
